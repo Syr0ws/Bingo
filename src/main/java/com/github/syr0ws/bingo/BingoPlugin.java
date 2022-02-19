1c0b417e731d71c6f4f675aee99ce729415624ce
Syrows <clement.gho@gmail.com>
2022-02-19 18:25:24 +0100

    Initializing project.

---
+++ src/main/java/com/github/syr0ws/bingo/BingoPlugin.java
@@ -0,0 +1,16 @@
+package com.github.syr0ws.bingo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BingoPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
