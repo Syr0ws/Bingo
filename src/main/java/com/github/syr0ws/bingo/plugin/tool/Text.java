a283f4a291fc2b4b085d70c64826e3ef0534a158
Syrows <clement.gho@gmail.com>
2022-02-22 20:36:04 +0100

    Adding messages.

--- src/main/java/com/github/syr0ws/bingo/plugin/tool/Text.java
+++ src/main/java/com/github/syr0ws/bingo/plugin/tool/Text.java
@@ -4,7 +4,9 @@ import com.github.syr0ws.bingo.plugin.util.TextUtil;
 
 public enum Text {
 
-    BINGO_INVENTORY_TITLE("&6Bingo");
+    BINGO_INVENTORY_TITLE("&6Bingo"),
+    GAME_STARTING_IN("&eDébut dans &6%d&e."),
+    GAME_STARTED("&6Début de la partie !");
 
     private final String text;
 
