249fd94ebcf29331fd25c2eab70704fef286bbc4
Syrows <clement.gho@gmail.com>
2022-02-22 13:30:50 +0100

    Handling found items.

--- src/main/java/com/github/syr0ws/bingo/plugin/game/listener/GameRunningListener.java
+++ src/main/java/com/github/syr0ws/bingo/plugin/game/listener/GameRunningListener.java
@@ -1,11 +1,18 @@
 package com.github.syr0ws.bingo.plugin.game.listener;
 
 import com.github.syr0ws.bingo.api.game.model.GameModel;
+import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.UUID;
 
 public class GameRunningListener implements Listener {
 
@@ -22,10 +29,57 @@ public class GameRunningListener implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerPickupItem(EntityPickupItemEvent event) {
 
+        Entity entity = event.getEntity();
+        Item item = event.getItem();
+        ItemStack stack = item.getItemStack();
+
+        if(!(entity instanceof Player player)) return;
+
+        boolean handled = this.handleItemFound(player, stack);
+
+        if(!handled) return;
+
+        if(stack.getAmount() > 1) {
+
+            stack.setAmount(stack.getAmount() - 1);
+            item.setItemStack(stack);
+
+        } else item.remove();
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerCraftItem(CraftItemEvent event) {
 
+        Player player = (Player) event.getWhoClicked();
+        ItemStack item = event.getCurrentItem();
+
+        if(item == null) return;
+
+        boolean handled = this.handleItemFound(player, item);
+
+        if(!handled) return;
+
+        if(item.getAmount() > 1) {
+
+            item.setAmount(item.getAmount() - 1);
+            event.setCurrentItem(item);
+
+        } else event.setCurrentItem(null);
+    }
+
+    private boolean handleItemFound(Player player, ItemStack item) {
+
+        UUID uuid = player.getUniqueId();
+        Material material = item.getType();
+
+        if(!this.model.hasPlayer(uuid))
+            return false;
+
+        if(!this.model.getGrid().contains(material))
+            return false;
+
+        this.model.setFoundItem(uuid, material);
+
+        return true;
     }
 }
