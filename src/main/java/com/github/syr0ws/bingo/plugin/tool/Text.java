691248e9547e02cf2f679eac79e3114e8f24385c
Syrows <clement.gho@gmail.com>
2022-02-23 13:45:25 +0100

    Messages are now in english.

--- src/main/java/com/github/syr0ws/bingo/plugin/tool/Text.java
+++ src/main/java/com/github/syr0ws/bingo/plugin/tool/Text.java
@@ -5,18 +5,18 @@ import com.github.syr0ws.bingo.plugin.util.TextUtil;
 public enum Text {
 
     BINGO_INVENTORY_TITLE("&6Bingo"),
-    GAME_STARTING_IN("&eDébut dans &6%d&e."),
-    GAME_STARTED("&6Début de la partie !"),
-    ITEM_FOUND("&6%s &ea trouvé un item !"),
-    ROW_COMPLETE("&6%s &ea complété une ligne !"),
-    COLUMN_COMPLETE("&6%s &ea complété une colonne !"),
-    DIAGONAL_COMPLETE("&6%s &ea complété une diagonale !"),
-    PLAYER_BINGO_COMPLETE("&6%s &ea complété son bingo en entier !"),
-    PLAYER_WIN("&6%s &eremporte la partie !"),
-    GAME_TIME_LEFT_MINUTE("&eIl reste &6%d minute%s &epour compléter votre bingo."),
-    GAME_TIME_LEFT_SECOND("&eIl reste &6%d seconde%s &epour compléter votre bingo."),
-    RESTART("&cLe serveur redémarre. Veuillez vous reconnecter."),
-    GAME_FINISHED("&cLa partie est terminée !");
+    GAME_STARTING_IN("&eGame starting in &6%d&e."),
+    GAME_STARTED("&6Starting the game !"),
+    ITEM_FOUND("&6%s &ehas found an item !"),
+    ROW_COMPLETE("&6%s &ehas completed a line !"),
+    COLUMN_COMPLETE("&6%s &ehas completed a column !"),
+    DIAGONAL_COMPLETE("&6%s &ehas completed a diagonal !"),
+    PLAYER_BINGO_COMPLETE("&6%s &ehas completed his bingo grid !"),
+    PLAYER_WIN("&6%s &ewins the game !"),
+    GAME_TIME_LEFT_MINUTE("&eThere are &6%d minute%s &eleft to complete your bingo."),
+    GAME_TIME_LEFT_SECOND("&eThere are &6%d second%s &eleft to complete your bingo."),
+    RESTART("&cThe server is restarting. Please reconnect."),
+    GAME_FINISHED("&cThe game is finished !");
 
     private final String text;
 
